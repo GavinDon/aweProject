87a6c093b20cc499e0dcd75787c37e6702e70832
李南 <50893850@qq.com>
2018-03-27 18:08:44 +0800

    打造个人的网址

--- aweProject/urls.py
+++ aweProject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('myblog.urls')),
     path('admin/', admin.site.urls),
+    path('myblog/', include('myblog.urls', namespace='myblog')),
 ]
